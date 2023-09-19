12dbc00265d1be8a51530ee87f0c286cd3702f26
christoferNK22 <christofernathanael116@gmail.com>
2023-09-19 14:50:13 +0700

    fix(layout): sidebar icon

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -23,18 +23,18 @@
                 </li>
                 <li class="menu-header">Master</li>
                 <li class="dropdown">
-                    <a class="nav-link" href="{{ route('master.church.index') }}"><i class="far fa-square"></i>
+                    <a class="nav-link" href="{{ route('master.church.index') }}"><i class="fas fa-church"></i>
                         <span>Gereja</span></a>
-                    <a class="nav-link" href="{{ route('master.interest.index') }}"><i class="far fa-square"></i>
+                    <a class="nav-link" href="{{ route('master.interest.index') }}"><i class="fas fa-briefcase"></i>
                         <span>Bidang
                             Minat</span></a>
-                    <a class="nav-link" href="{{ route('master.education.index') }}"><i class="far fa-square"></i>
+                    <a class="nav-link" href="{{ route('master.education.index') }}"><i class="fas fa-school"></i>
                         <span>Edukasi</span></a>
                 </li>
 
                 <li class="menu-header">Main Page</li>
                 <li class="dropdown">
-                    <a class="nav-link" href="{{ route('master.member.index') }}"><i class="far fa-square"></i>
+                    <a class="nav-link" href="{{ route('master.member.index') }}"><i class="fas fa-users"></i>
                         <span>Member</span></a>
                 </li>
             @endif
