e507bddda94133a718eb6d5468112943d2f9ed8a
christoferNK22 <christofernathanael116@gmail.com>
2023-11-11 23:55:00 +0700

    add: new migration for CMS

---
+++ database/migrations/2023_11_10_141608_create_cms_management_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cms_management', function (Blueprint $table) {
+            $table->id();
+            $table->enum('dewan', ['penasihat', 'pakar', 'pengurus']);
+            $table->enum('position', 
+                        ['ketua', 'wakil_ketua', 'sekertaris', 'wakil_sekertaris', 
+                        'bendahara', 'wakil_bendahara', 'anggota']);
+            $table->string('name')->default('');
+            $table->string('origin')->default('');
+            $table->string('image')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cms_management');
+    }
+};
