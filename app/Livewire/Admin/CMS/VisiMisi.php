8d8fad1475b413285e526e128f4aad9647951cce
christoferNK22 <christofernathanael116@gmail.com>
2023-11-23 15:47:07 +0700

    fix: livewire visi misi

--- app/Livewire/Admin/CMS/VisiMisi.php
+++ app/Livewire/Admin/CMS/VisiMisi.php
@@ -18,14 +18,26 @@ class VisiMisi extends Component
         return view('livewire.admin.cms.visi-misi');
     }
 
+    public function mount()
+    {
+        $visiMisi = CmsVisiMisi::first();
+        if (!empty($visiMisi)) {
+            $this->visi1 = $visiMisi->visi1 ?? "";
+            $this->visi2 = $visiMisi->visi2 ?? "";
+            $this->visi3 = $visiMisi->visi3 ?? "";
+            $this->visi4 = $visiMisi->visi4 ?? "";
+            $this->visi5 = $visiMisi->visi5 ?? "";
+        }
+    }
+
     public function save()
     {
         $model = new CmsVisiMisi();
         $model->visi1 = $this->visi1;
-        $model->visi1 = $this->visi1;
-        $model->visi1 = $this->visi1;
-        $model->visi1 = $this->visi1;
-        $model->visi1 = $this->visi1;
+        $model->visi2 = $this->visi2;
+        $model->visi3 = $this->visi3;
+        $model->visi4 = $this->visi4;
+        $model->visi5 = $this->visi5;
         $model->save();
 
         return redirect(route('cms.visi-misi.index'));
