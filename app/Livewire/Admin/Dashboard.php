cef02c7628be8b43005a8865a58023cb74013dc5
Richie-Z <zakariarichie@yahoo.com>
2023-11-24 15:27:57 +0700

    feat(dashboard): functionality in admin dashboard

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,12 +2,46 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Church;
+use App\Models\CMS\CmsManagement;
+use App\Models\Member;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public function render()
+    public int $newMember = 0;
+    public int $oldMember = 0;
+    public int $sumPengurus = 0;
+    public int $sumChurch = 0;
+
+    public string $churchId  = "";
+
+    public function mount()
     {
-        return view('livewire.admin.dashboard');
+        $this->newMember = Member::where('is_verified', false)->get()->count();
+        $this->oldMember = Member::where('is_verified', true)->get()->count();
+        $this->sumPengurus = CmsManagement::all()->count();
+        $this->sumChurch = Church::all()->count();
+    }
+
+    public function calculateSumMember()
+    {
+        $member = Member::query();
+        if (!empty($this->churchId)) {
+            $member->where('church_id', $this->churchId);
+        }
+        $this->newMember = $member->where('is_verified', false)->get()->count();
+
+        $member  = Member::query();
+        if (!empty($this->churchId)) {
+            $member->where('church_id', $this->churchId);
+        }
+        $this->oldMember = $member->where('is_verified', true)->get()->count();
+    }
+
+    public function render(): View
+    {
+        return view('livewire.admin.dashboard', ['churches' => Church::all()]);
     }
 }
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,49 +1,84 @@
 <div>
     <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div class="card card-statistic-1">
-            <div class="card-icon bg-primary">
-              <i class="far fa-user"></i>
-            </div>
-            <div class="card-wrap">
-              <div class="card-header">
-                <h4>Total Member</h4>
-              </div>
-              <div class="card-body">
-                10
-              </div>
-            </div>
-          </div>
+        <div class="col-12">
+            <form action="">
+                <div class="form-group">
+                    <label for="">Gereja</label>
+                    <select name="" id="" class="form-control" wire:model.live="churchId"
+                        wire:change="calculateSumMember">
+                        <option value="" selected>Semua Gereja</option>
+                        @foreach ($churches as $church)
+                            <option value="{{ $church->id }}">{{ $church->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </form>
         </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div class="card card-statistic-1">
-            <div class="card-icon bg-danger">
-              <i class="far fa-newspaper"></i>
-            </div>
-            <div class="card-wrap">
-              <div class="card-header">
-                <h4>Total Pengurus</h4>
-              </div>
-              <div class="card-body">
-                42
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div class="card card-statistic-1">
-            <div class="card-icon bg-warning">
-              <i class="far fa-file"></i>
-            </div>
-            <div class="card-wrap">
-              <div class="card-header">
-                <h4>Member Baru</h4>
-              </div>
-              <div class="card-body">
-                1,201
-              </div>
-            </div>
-          </div>
-        </div>                
     </div>
+
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                    <i class="far fa-newspaper"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Pengurus</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $sumPengurus }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-warning">
+                    <i class="far fa-file"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Gereja</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $sumChurch }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="card card-statistic-2">
+                <div class="card-stats">
+                    <div class="card-stats-title">
+                    </div>
+                    <div class="card-stats-items">
+                        <div class="card-stats-item">
+                            <div class="card-stats-item-count">{{ $newMember }}</div>
+                            <div class="card-stats-item-label">Baru</div>
+                        </div>
+                        <div class="card-stats-item">
+                            <div class="card-stats-item-count">{{ $oldMember }}</div>
+                            <div class="card-stats-item-label">Lama</div>
+                        </div>
+                        <div class="card-stats-item">
+                            <div class="card-stats-item-count">{{ $newMember + $oldMember }}</div>
+                            <div class="card-stats-item-label">Total</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-archive"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Member</h4>
+                    </div>
+                    <div class="card-body">{{ $newMember + $oldMember }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
